28f1b72caae5ef4182f3be684d3b8a97348d74f7
arvind.m.vepa <arvind.m.vepa@gmail.com>
2020-05-31 04:34:18 -0700

    bug

--- 3DNet/pred_train_TReNDs.py
+++ 3DNet/pred_train_TReNDs.py
@@ -16,7 +16,7 @@ def test_features(data_loader, model, sets, save_path):
     # settings
     print("validation")
     model.eval()
-    feature_extractor = torch.nn.Sequential(list(model.children())[-2])
+    feature_extractor = torch.nn.Sequential(list(model.modules())[-1])
 
     y_features = []
     ids_all = []
