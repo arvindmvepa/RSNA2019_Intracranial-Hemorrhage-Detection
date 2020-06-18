ca92947ef92f0dc20382a3a98fd72420e7ba358c
arvind.m.vepa <arvind.m.vepa@gmail.com>
2020-06-17 19:25:02 -0700

    update

--- 3DNet/model.py
+++ 3DNet/model.py
@@ -42,36 +42,40 @@ def generate_model(opt):
             )
         elif opt.model_depth == 50:
             model = resnet.resnet50(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
+                #sample_input_W=opt.input_W,
+                #sample_input_H=opt.input_H,
+                #sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
-                num_seg_classes=opt.n_seg_classes)
+                #num_seg_classes=opt.n_seg_classes
+            )
         elif opt.model_depth == 101:
             model = resnet.resnet101(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
+                #sample_input_W=opt.input_W,
+                #sample_input_H=opt.input_H,
+                #sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
-                num_seg_classes=opt.n_seg_classes)
+                #num_seg_classes=opt.n_seg_classes
+            )
         elif opt.model_depth == 152:
             model = resnet.resnet152(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
+                #sample_input_W=opt.input_W,
+                #sample_input_H=opt.input_H,
+                #sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
-                num_seg_classes=opt.n_seg_classes)
+                #num_seg_classes=opt.n_seg_classes
+            )
         elif opt.model_depth == 200:
             model = resnet.resnet200(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
+                #sample_input_W=opt.input_W,
+                #sample_input_H=opt.input_H,
+                #sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
-                num_seg_classes=opt.n_seg_classes)
+                #num_seg_classes=opt.n_seg_classes
+            )
     
     if not opt.no_cuda:
             model = model.cuda()
