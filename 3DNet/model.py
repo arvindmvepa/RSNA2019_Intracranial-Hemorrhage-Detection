803497c938f19fe8476a768dd1808b65b7fdc2df
arvind.m.vepa <arvind.m.vepa@gmail.com>
2020-06-17 18:32:18 -0700

    update

--- 3DNet/model.py
+++ 3DNet/model.py
@@ -24,12 +24,13 @@ def generate_model(opt):
             )
         elif opt.model_depth == 18:
             model = resnet.resnet18(
-                sample_input_W=opt.input_W,
-                sample_input_H=opt.input_H,
-                sample_input_D=opt.input_D,
+                #sample_input_W=opt.input_W,
+                #sample_input_H=opt.input_H,
+                #sample_input_D=opt.input_D,
                 shortcut_type=opt.resnet_shortcut,
                 no_cuda=opt.no_cuda,
-                num_seg_classes=opt.n_seg_classes)
+                #num_seg_classes=opt.n_seg_classes
+            )
         elif opt.model_depth == 34:
             model = resnet.resnet34(
                 sample_input_W=opt.input_W,
